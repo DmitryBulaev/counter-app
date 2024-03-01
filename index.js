dc5e0cacd14a21f60f0ffa0a06dca0a0963b6086
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-01 20:15:07 +0500

    update files

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let button = document.querySelector('button');
-
+let counterText = document.querySelector('.counter');
 let counter = 0;
 
 button.addEventListener('click', function() {
 
     counter = counter + 1;
     
-    console.log(counter);
+    counterText.innerText = counter;
 });
+
