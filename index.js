3a7f13497f3a71fe40ad81cabd2bab6c19034a21
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-02 19:16:47 +0500

    added draft reset button

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ button.addEventListener('click', function() {
     counterText.innerText = counter;
 });
 
+// let button = document.querySelector('.');
+// let counterText = document.querySelector('.counter');
+// let counter = 0;
+
+button.addEventListener('click', function() {
+
+    counter = counter + 1;
+    
+    counterText.innerText = counter;
+});
