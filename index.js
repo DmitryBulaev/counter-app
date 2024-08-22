dd563ee57b965fb6deb87c195d26637282adc076
Dmitry Bulaev <bulaevd74@mail.ru>
2024-08-22 14:10:37 +0500

    refactoring

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const counterElement = document.querySelector('[data-find="counterValue"]');
-const addApproachButton  = document.getElementById("addApproachButton");
+const counterText = document.querySelector("[data-value = counterValue]");
+const addSetButton = document.getElementById("addSetButton");
 const resetButton = document.getElementById("resetButton");
 
 const COUNTER_INITIAL_VALUE = 0;
+counterText.innerText = COUNTER_INITIAL_VALUE;
 
 let counter = COUNTER_INITIAL_VALUE;
-counterElement.innerText = counter;
 
-addApproachButton.addEventListener("click", function () {
-  counter = counter + parseInt(addApproachButton.dataset.approach);
-  counterElement.innerText = counter;
+addSetButton.addEventListener("click", function () {
+  counter = counter + parseInt(addSetButton.dataset.sets);
+  counterText.innerText = counter;
 });
 
 resetButton.addEventListener("click", function () {
   counter = COUNTER_INITIAL_VALUE;
-  counterElement.innerText = counter;
+  counterText.innerText = counter;
 });
